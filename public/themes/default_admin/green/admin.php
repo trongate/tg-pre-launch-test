4e36debc9f251b76c7962e28cabbeea5882af352
David <david.webguy@gmail.com>
2021-03-19 17:48:41 +0000

    better CSS

--- public/themes/default_admin/green/admin.php
+++ public/themes/default_admin/green/admin.php
@@ -1,59 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?= BASE_URL ?>css/trongate.css">
-    <link rel="stylesheet" href="<?= BASE_URL ?>css/admin.css">
-    <link rel="stylesheet" href="<?= BASE_URL ?>css/trongate-datetime.css">
-    <link rel="stylesheet" href="<?= THEME_DIR ?>css/admin.css"> 
-    <?= $additional_includes_top ?>
-    <title>Admin</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
 </head>
 <body>
-<header>
-    <nav class="hide-on-sm">
-        <ul>
-            <li><?= anchor('#', 'Logo') ?></li>
-            <li><?= anchor('#', 'About') ?></li>
-            <li><?= anchor('#', 'Values') ?></li>
-            <li><?= anchor('#', 'News') ?></li>
-            <li><?= anchor('#', 'Contact') ?></li>
-            <li><?= anchor('#', 'Clients') ?></li>
-            <li><?= anchor('#', 'Partners') ?></li>
-        </ul>        
-    </nav>
-    <div class="hide-on-sm"><?= anchor('#', 'Admin Users') ?></div>
-    <div id="hamburger" class="hide-on-lg" onclick="openSideNav()">&#9776;</div>
-</header>
-<div class="wrapper">
-    <div id="sidebar">
-        <h3>Menu</h3>
-        <nav id="left-nav">
-            <?= Template::partial('partials/admin/dynamic_nav') ?>
-        </nav>       
-    </div>
-    <div>
-        <main><?= Template::display($data) ?></main>
-        <footer>
-            <div>Footer</div>
-            <div>Powered by <?= anchor('https://trongate.io', 'Trongate') ?></div>
-        </footer>
-    </div>
-</div>
-<div id="side-nav">
-    <div id="close-btn" onclick="closeSideNav()">&times;</div>
-    <ul>
-        <li><a href="https://speedcodingacademy.com/">Home</a></li>
-        <li><a href="https://speedcodingacademy.com/contactus">Get In Touch</a></li>
-        <li><a href="https://speedcodingacademy.com/members/login">Members' Login</a></li>
-    </ul>
-</div>
+	<header>header</header>
+	<div class="wrapper">
+	    <div id="sidebar">
+	        <h3>Menu</h3>
+	        <nav id="left-nav">
+	            <?= Template::partial('partials/admin/dynamic_nav') ?>
+	        </nav>       
+	    </div>
+	    <div>
+	        <main><?= Template::display($data) ?></main>
+	        <footer>
+	            <div>Footer</div>
+	            <div>Powered by <?= anchor('https://trongate.io', 'Trongate') ?></div>
+	        </footer>
+	    </div>	
+	</div>
+	<style>
+		body {
+			margin: 0;
+			min-height: 100vh;
+			display: flex;
+			flex-direction: column;
+		}
 
+		.wrapper {
+			flex: 1;
+			display: flex;
+			flex-direction: row;
+		}
 
-<script src="<?= BASE_URL ?>js/admin.js"></script>
-<script src="<?= BASE_URL ?>js/trongate-datetime.js"></script>
-<?= $additional_includes_btm ?>
+	    .wrapper > div:nth-child(1) {
+	        display: block;
+	        background-color: #eee;
+	        min-width: 250px;
+	    }
+
+		.wrapper > div:nth-child(2) {
+			flex: 1;
+		    display: flex;
+		    flex-direction: column;
+		    justify-content: space-between;
+		}
+	</style>
 </body>
 </html>
