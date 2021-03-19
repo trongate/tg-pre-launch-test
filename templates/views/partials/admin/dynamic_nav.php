f48fe2b2deef9ed1bb6ccc00e6fa502a613f3a04
David <david.webguy@gmail.com>
2021-03-19 00:38:58 +0000

    I think we are good

--- templates/views/partials/admin/dynamic_nav.php
+++
@@ -1,5 +0,0 @@
-<ul>
-    <li><?= anchor('drivers/manage', '<i class="fa fa-universal-access"></i> Manage Drivers') ?></li>
-    <li><?= anchor('licenses/manage', '<i class="fa fa-universal-access"></i> Manage Licenses') ?></li>
-    <li><?= anchor('messagess/manage', 'Manage Messagess') ?></li>
-</ul>
--- templates/viewsORIG/admin_files/dynamic_nav.php
+++
@@ -1,2 +0,0 @@
-
-<a class="w3-bar-item w3-button w3-hover-black" href="<?= BASE_URL ?>teams/manage"><i class="fa fa-universal-access"></i> Manage Teams</a>
