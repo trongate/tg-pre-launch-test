ad80deb1d11b106c173ea92413585b729d344c09
David <david.webguy@gmail.com>
2021-03-19 19:04:08 +0000

    headlines looking good

--- public/themes/default_admin/green/admin.php
+++ public/themes/default_admin/green/admin.php
@@ -1,12 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>css/trongate.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>css/admin.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>css/trongate-datetime.css">
+    <link rel="stylesheet" href="<?= THEME_DIR ?>css/admin.css">
+    <?= $additional_includes_top ?>
+    <title>Admin</title>
 </head>
 <body>
-	<header>header</header>
+	<header>	
+	    <nav class="hide-sm">
+	        <ul>
+	            <li><?= anchor('#', 'Logo') ?></li>
+	            <li><?= anchor('#', 'About') ?></li>
+	            <li><?= anchor('#', 'Values') ?></li>
+	            <li><?= anchor('#', 'News') ?></li>
+	            <li><?= anchor('#', 'Contact') ?></li>
+	            <li><?= anchor('#', 'Clients') ?></li>
+	            <li><?= anchor('#', 'Partners') ?></li>
+	        </ul>        
+	    </nav>
+	    <div class="hide-sm"><?= anchor('#', 'Admin Users') ?></div>
+	    <div id="hamburger" class="hide-lg" onclick="openSideNav()">&#9776;</div>
+	</header>
 	<div class="wrapper">
 	    <div id="sidebar">
 	        <h3>Menu</h3>
@@ -15,39 +35,19 @@
 	        </nav>       
 	    </div>
 	    <div>
-	        <main><?= Template::display($data) ?></main>
+	        <main>
+	        	<h1>H1 Headline</h1>
+	        	<h2>H2 Headline</h2>
+	        	<h3>H3 Headline</h3>
+	        	<h4>H4 Headline</h4>
+	        	<h5>H5 Headline</h5>
+	        	<h6>H6 Headline</h6>
+	        	<?= Template::display($data) ?></main>
 	        <footer>
 	            <div>Footer</div>
 	            <div>Powered by <?= anchor('https://trongate.io', 'Trongate') ?></div>
 	        </footer>
 	    </div>	
 	</div>
-	<style>
-		body {
-			margin: 0;
-			min-height: 100vh;
-			display: flex;
-			flex-direction: column;
-		}
-
-		.wrapper {
-			flex: 1;
-			display: flex;
-			flex-direction: row;
-		}
-
-	    .wrapper > div:nth-child(1) {
-	        display: block;
-	        background-color: #eee;
-	        min-width: 250px;
-	    }
-
-		.wrapper > div:nth-child(2) {
-			flex: 1;
-		    display: flex;
-		    flex-direction: column;
-		    justify-content: space-between;
-		}
-	</style>
 </body>
 </html>
