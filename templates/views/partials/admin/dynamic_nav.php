772e775a9148c1f001b7ee6018a756f79452b712
David <david@david-ThinkPad-X230>
2021-04-17 01:05:21 +0100

    Update license.txt, admin.css, and 3 more files...

--- templates/views/partials/admin/dynamic_nav.php
+++ templates/views/partials/admin/dynamic_nav.php
@@ -1,5 +1,2 @@
 <ul>
-    <li><?= anchor('cars/manage', 'Manage Cars') ?></li>
-    <li><?= anchor('cars/manage', 'Test Link 1') ?></li>
-    <li><?= anchor('cars/manage', 'Test Link 2') ?></li>
 </ul>
--- todo.php
+++ todo.php
@@ -1,15 +1,10 @@
-excluse trongate_ tables from vibe .
-
-
 *******************************************************
 *******************************************************
 *******************************************************
-*** UPLOADERS, DATE/TIME PICKER, RELATIONS  ***********
+********************* RELATIONS  **********************
 *******************************************************
 *******************************************************
 *******************************************************
-
-
 
 --------------------------------------
 8). (finish crypto payment gateway integration) - create new address for * buys  
@@ -21,4 +16,14 @@ excluse trongate_ tables from vibe .
 12. Beautification.  
 13. Alerts.  
 FIN.
+
+
+
+
+TWEAKS
+
+* get nav icons working
+* add highlight for selected date.
+* on date range picker make sure calendar returns to selected date upon update.
+* test make sure returns to selected date for ordinary calendar popups and datetime popups.
 
